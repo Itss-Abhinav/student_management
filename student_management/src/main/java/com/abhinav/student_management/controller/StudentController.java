846633dc7e81ddbc44ce3f4dc65ce6ea0996dfb0
Abhinav M <abhinavm0489@gmail.com>
2026-06-19 16:06:15 +0530

    update crossorigin to vercel url

--- student_management/src/main/java/com/abhinav/student_management/controller/StudentController.java
+++ student_management/src/main/java/com/abhinav/student_management/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins = "*") // ← add this
+@CrossOrigin(origins = "https://student-app-pied.vercel.app")// ← add this
 @RestController
 @RequestMapping("/api/students")
 
