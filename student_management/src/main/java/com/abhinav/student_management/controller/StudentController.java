737aaf89f42a113c07a9d67b92937e87a9dbafdc
Abhinav M <abhinavm0489@gmail.com>
2026-06-19 15:57:22 +0530

    allow all origins

--- student_management/src/main/java/com/abhinav/student_management/controller/StudentController.java
+++ student_management/src/main/java/com/abhinav/student_management/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")   // ← add this
+@CrossOrigin(origins = "*") // ← add this
 @RestController
 @RequestMapping("/api/students")
 
