8cea3f50f81406c3b974b0637b51c7dd170c904f
Abhinav M <abhinavm0489@gmail.com>
2026-06-19 15:00:44 +0530

    add crossorigin and fixes

--- student_management/src/main/java/com/abhinav/student_management/controller/StudentController.java
+++ student_management/src/main/java/com/abhinav/student_management/controller/StudentController.java
@@ -7,8 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")   // ← add this
 @RestController
 @RequestMapping("/api/students")
+
+
+
+
+//@RestController
+//@RequestMapping("/api/students")
 public class StudentController {
 
     private final StudentService service;
